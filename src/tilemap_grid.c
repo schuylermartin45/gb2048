71aa77c881e91221a639642d0441f8e41009e67a
Schuyler Martin <schuylermartin45@gmail.com>
2022-09-04 09:11:07 -0600

    Forgotten diff again
    - VS Code auto save was disabled

--- src/tilemap_grid.c
+++ src/tilemap_grid.c
@@ -8,7 +8,6 @@
    Section       : 
    Bank          : 0
    Map size      : 20 x 18
-   Tile set      : Z:\home\schuyler\projects\gameboy_dev\gb2048\img\gb2048_tiles.gbr
    Plane count   : 1 plane (8 bits)
    Plane order   : Tiles are continues
    Tile offset   : 0
